4b62dd44347bbd16ca643614b75e67dd7a50a637
iamarvy <okeseyi5@gmail.com>
2025-07-02 18:27:01 +0100

    Add duplicate EXPLAIN and SELECT statements for performance analysis in database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,13 @@
+EXPLAIN
+SELECT
+  *
+FROM
+  Bookings
+  JOIN Users ON Bookings.user_id = Users.id
+  JOIN Properties ON Bookings.property_id = Properties.id
+WHERE
+  Bookings.date > '2024-01-01';
+
 -- Indexes for Users table
 CREATE INDEX idx_users_email ON Users (email);
 
@@ -12,3 +22,13 @@ CREATE INDEX idx_bookings_date ON Bookings (date);
 CREATE INDEX idx_properties_location ON Properties (location);
 
 CREATE INDEX idx_properties_name ON Properties (name);
+
+EXPLAIN
+SELECT
+  *
+FROM
+  Bookings
+  JOIN Users ON Bookings.user_id = Users.id
+  JOIN Properties ON Bookings.property_id = Properties.id
+WHERE
+  Bookings.date > '2024-01-01';
